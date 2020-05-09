ff24dd381efaa93a9775446252972d55e3a1ba75
dut-a <d.dut@live.com>
2020-05-09 16:09:40 -0500

    App base structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          We shall come here with our swords sharpened properly to tackle some Math problems...
+          Keep watching this space...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
