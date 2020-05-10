46a03d392a8a294c3522270c11d6d9c31e0f9c6b
dut-a <d.dut@live.com>
2020-05-10 15:36:48 -0500

    Adds Keypad layout markup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          We shall come here with our swords sharpened properly to tackle some Math problems...
-          Keep watching this space...
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = React.createClass({  
+  render() {    
+    return (
+      <div className="mighty-calc">
+        <SetupKeypadNumbers />    
+      </div>
+    )
+  }
+});
+const Button = React.createClass({
+  handleClick() {
+    let text = this.props.text;
+    let clickBox = this.props.clickHandler;
+    if (clickBox) { clickBox.call(null, text); }
+  },
+  
+  render() {
+    return (
+      <button className={this.props.kirass} onClick={this.handleClick}>
+        <span className="title">{this.props.text}</span>
+      </button>
+    );
+  }
+});
+const SetupKeypadNumbers = React.createClass({
+  customNumber = num => {
+    if (!store.curInput) {
+      return store.newInput = num;
+    }
+    
+    return store.newInput = `${store.curInput}${num}`;
+  },
+  
+  render() {
+    return (
+      <section className="btn-set-nums">
+        <Button text="1" clickHandler={this.customNumber} />
+        <Button text="2" clickHandler={this.customNumber} />
+        <Button text="3" clickHandler={this.customNumber} />
+        <Button text="4" clickHandler={this.customNumber} />
+        <Button text="5" clickHandler={this.customNumber} />
+        <Button text="6" clickHandler={this.customNumber} />
+        <Button text="7" clickHandler={this.customNumber} />
+        <Button text="8" clickHandler={this.customNumber} />
+        <Button text="9" clickHandler={this.customNumber} />
+        <Button text="0" clickHandler={this.customNumber} />
+      </section>
+    )
+  }
+});
 
-export default App;
+React.render(
+  <App />,
+  document.querySelector('#root')
+);
+
+
+
